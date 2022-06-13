4ec2608131a5d2b6eca6992248aafb1aeb58fc97
Anh Vo <anhvo@anhs-mbp.myfiosgateway.com>
2022-06-12 20:18:09 -0400

    Remove build files

--- exercise_1/target/debug/deps/exercise_1-019a1445f4756496.d
+++
@@ -1,5 +0,0 @@
-/Users/anhvo/Documents/Learning/Rust_Exercises/exercise_1/target/debug/deps/exercise_1-019a1445f4756496.rmeta: src/main.rs
-
-/Users/anhvo/Documents/Learning/Rust_Exercises/exercise_1/target/debug/deps/exercise_1-019a1445f4756496.d: src/main.rs
-
-src/main.rs:
--- exercise_1/target/debug/exercise_1.d
+++
@@ -1,1 +0,0 @@
-/Users/anhvo/Documents/Learning/Rust_Exercises/exercise_1/target/debug/exercise_1: /Users/anhvo/Documents/Learning/Rust_Exercises/exercise_1/src/main.rs
